e3ec31ffd32e3eec05c06108b29bdc03ad97654f
Vahe Mikayelyan <vahemikayelyan@gmail.com>
2023-11-01 20:37:38 -0700

    red

--- components/InputGroup.tsx
+++ components/InputGroup.tsx
@@ -19,25 +19,23 @@ const InputGroup: React.FC<Props> = ({
   autoComplete = autoComplete || "off";
 
   return (
-    <>
-      <div>
-        <label
-          htmlFor={name}
-          className="block mb-2 text-sm font-medium text-gray-900"
-        >
-          {title}
-        </label>
-        <input
-          id={name}
-          name={name}
-          type={type}
-          placeholder={placeholder}
-          autoComplete={autoComplete}
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-        />
-      </div>
-      {error && <small className="text-red-600">{error}</small>}
-    </>
+    <div>
+      <label
+        htmlFor={name}
+        className="block mb-2 text-sm font-medium text-gray-900"
+      >
+        {title}
+      </label>
+      <input
+        id={name}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        autoComplete={autoComplete}
+        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+      />
+      {error && <p className="mt-2 text-sm text-red-600">{error}</p>}
+    </div>
   );
 };
 
