d9014fc71b7ad689aa66e3236cf5db943bcd7762
Vahe Mikayelyan <vahemikayelyan@gmail.com>
2023-11-01 20:31:56 -0700

    red color

--- components/InputGroup.tsx
+++ components/InputGroup.tsx
@@ -36,7 +36,7 @@ const InputGroup: React.FC<Props> = ({
           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
         />
       </div>
-      {error && <small className="text-red-500">{error}</small>}
+      {error && <small className="text-red-600">{error}</small>}
     </>
   );
 };
