6270f8a82b09c5c43baa6c6b30f47d4d7c8d9b93
Vahe Mikayelyan <vahemikayelyan@gmail.com>
2023-11-19 13:35:37 -0800

    ddd

--- app/dashboard/AudioPlayer.tsx
+++ app/dashboard/AudioPlayer.tsx
@@ -1,29 +1,49 @@
-import { postRequest } from "@/utils/shared";
+import { useRef, useState } from "react";
+import OptionsModal from "./OptionsModal";
 
 const AudioPlayer = () => {
-  const handleFileChange = async (event: any) => {
-    const file = event.target.files[0];
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [popupDisplay, setPopupDisplay] = useState(false);
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const files = event.target.files;
 
-    if (file) {
-      const localAudioSrc = URL.createObjectURL(file);
-      const formData = new FormData();
-      formData.append("file", file)
-      const response = await postRequest('spleeter', formData, false)
+    if (files) {
+      const file = files[0];
 
-      console.log(response)
-
-      if (response) {
+      if (file) {
+        setPopupDisplay(true);
+        //const localAudioSrc = URL.createObjectURL(file);
+        //const formData = new FormData();
+        //formData.append("file", file)
+        //const response = await postRequest('spleeter', formData, false)
+        //console.log(response)
       }
     }
   };
 
-  return (
-    <div className="text-center mt-4"><input
-      type="file"
-      accept="*"
-      onChange={handleFileChange}
-    /></div>
-  )
-}
+  const closePopup = () => {
+    setPopupDisplay(false);
 
-export default AudioPlayer
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
+  };
+
+  return (
+    <>
+      <div className="text-center mt-4">
+        <input
+          accept="*"
+          type="file"
+          ref={fileInputRef}
+          onChange={handleFileChange}
+        />
+      </div>
+      {popupDisplay && <OptionsModal closePopup={closePopup} />}
+    </>
+  );
+};
+
+export default AudioPlayer;
--- components/UserMenu.tsx
+++ components/UserMenu.tsx
@@ -20,11 +20,7 @@ function UserMenu(props: DefaultSession) {
         className="flex relative mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
       >
         <span className="sr-only">Open user menu</span>
-        <img
-          className="w-8 h-8 rounded-full"
-          src="/docs/images/people/profile-picture-3.jpg"
-          alt="user photo"
-        />
+        <span className="w-8 h-8 rounded-full"></span>
       </button>
       <div
         className={`${
